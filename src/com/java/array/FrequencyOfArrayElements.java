a754bb121cf21b17126b8b3e5994f2a0c2200b39
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-26 11:23:54 +0530

    Added logic to find frequency of given array element

--- src/com/java/array/FrequencyOfArrayElements.java
+++ src/com/java/array/FrequencyOfArrayElements.java
@@ -18,41 +18,61 @@ public class FrequencyOfArrayElements
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter Size: ");
 		
-		//size of array
+		/*
+		 * size of array
+		 */
 		int size= input.nextInt();
 		
-		//Actual Array
+		/*
+		 * Actual Array
+		 */
 		int number[] = new int[size];
 		
-		//Array will store the frequency(number of times repeated)
+		/*
+		 * Array will store the frequency(number of times repeated)
+		 */
 		int frequency[] = new int[number.length];
 		
-		//visit=-1 bcoz initially it will not be pointing to any index or value of array
+		/*
+		 * visit=-1 bcoz initially it will not be pointing to any index or value of array
+		 */
 		int visit = -1;
 		
-		//Take Inputs array from user
+		/*
+		 * Take Inputs array from user
+		 */
 			System.out.println("Repeated numbers are allowed of length: "+size);
 			for(int i=0; i<number.length; i++) 
 			{
 				number[i] = input.nextInt();
 			}
-				//initial loop
+				/*
+				 * initial loop
+				 */
 				for(int i=0; i<number.length; i++)
 				{
-					//initial count will be one when it visits any number in array
+					/*
+					 * initial count will be one when it visits any number in array
+					 */
 					int count=1;
 					for(int j=i+1; j<number.length; j++)
 					{
-						//checking 0th index with 1st index
+						/*
+						 * checking 0th index with 1st index
+						 */
 						if(number[i] == number[j])
 						{
-							//if true count is increased and stored in freq[]
+							/*
+							 * if true count is increased and stored in freq[]
+							 */
 							count++;
 							frequency[j]=visit;
 						}
 								if(frequency[i] != visit)
 								{
-									//if frequency visit is not equal to -1 and prevents duplication
+									/*
+									 * if frequency visit is not equal to -1 and prevents duplication
+									 */
 									frequency[i] = count;
 								}
 			         }
@@ -62,7 +82,9 @@ public class FrequencyOfArrayElements
 					 {
 						if(frequency[i] !=  visit)
 						 {
-							//array element     number of times repeated
+							/*
+							 * array element     number of times repeated
+							 */
 							System.out.println(number[i]+"  \t\t"+frequency[i]);
 						}
 					 }
